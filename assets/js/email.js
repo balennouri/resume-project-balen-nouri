c854c31d6cc009567889c1ead8f5ab4071ad5649
balennouri <balennouri@gmail.com>
2024-01-20 16:45:55 +0100

    Add emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("Yservice_xcen2j1", "balen", {
+    emailjs.send("gmail","balennouri", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.emailaddress.value,
             "project_request": contactForm.projectsummary.value
