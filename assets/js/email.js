748884f7ec65738d11bd0890c64dc574557c3a5f
balennouri <balennouri@gmail.com>
2024-01-20 17:31:30 +0100

    Feat: Added send form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail","balennouri", {
+    emailjs.send("gmail", "balennouri", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.emailaddress.value,
             "project_request": contactForm.projectsummary.value
@@ -7,17 +7,10 @@ function sendMail(contactForm) {
         .then(
             function (response) {
                 console.log("SUCCESS", response);
-                // Show the thank you modal
-                $('#thankYouModal').modal('show');
-
-                // Event listener for modal close
-                $('#thankYouModal').on('hidden.bs.modal', function () {
-                    // Reset the form here
-                    contactForm.reset();
-                });
             },
             function (error) {
                 console.log("FAILED", error);
-            });
-    return false;
+            }
+        );
+    return false; // To block from loading a new page
 }
